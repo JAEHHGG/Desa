dc8bd537e328a20482177a4895bcd52d27eadea0
jhernan83075 <jhernan83075@universidadean.edu.co>
2019-04-30 08:09:06 -0500

    Añadido parrafo y header

--- Prueba-Web/index.php
+++ Prueba-Web/index.php
@@ -2,7 +2,7 @@
 <html lang="en">
 
 <head>
-     <meta charset="UTF-8">
+    <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Pruebas Desa</title>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
@@ -42,6 +42,16 @@
             </ul>
         </div>
     </nav>
+
+
+
+    <!-- Heade -->
+
+    <header>
+        <h1>Hola Mundo</h1>
+    </header>
+
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatem esse iure ipsam delectus, quo, temporibus qui perferendis ut, sit odio quibusdam ad eius sed cum quaerat, suscipit fugiat illo quia distinctio. Veritatis pariatur, quod, adipisci magnam aliquam quis assumenda sint inventore molestias similique ea odio. Consequatur libero velit quos porro maiores cum non quae quam necessitatibus amet accusantium maxime ad reprehenderit excepturi odit cupiditate, laudantium labore, est ab nisi. Provident quas eveniet, et maxime illo ducimus magnam, a ut, in, consequatur id! Ipsa esse maiores ab reiciendis, unde officiis nihil, repudiandae saepe neque alias debitis, vel cum aliquid at fuga veniam! Debitis, mollitia quia amet sapiente numquam accusantium quaerat, molestiae et excepturi iste. Adipisci expedita qui rem aliquam odit reprehenderit facilis? Quod dolor suscipit rerum officia iure blanditiis, eos vero nisi repellendus nam distinctio rem similique, odit tenetur ex obcaecati expedita cum libero mollitia illum aliquam sunt doloribus debitis, dicta. Recusandae, nihil. Nobis optio commodi beatae labore eum at possimus sed, iure enim quam praesentium, error consequatur veritatis est totam magnam vitae facere officia, libero tenetur maxime id! Delectus perferendis obcaecati eligendi animi maiores voluptatem similique hic ex consequatur laboriosam cum molestias magnam assumenda, eaque placeat consectetur aut in culpa, fuga deserunt libero. Sapiente ipsam perspiciatis hic! Iste maiores, ipsam provident possimus delectus assumenda quae doloribus esse molestiae in autem nostrum dignissimos iusto! Voluptatibus ipsam sint, est soluta esse, vitae iste eos culpa placeat dolorum, natus repellat cupiditate nesciunt rem. Itaque necessitatibus nihil, accusantium possimus quam, vitae fugit. Neque sed doloremque rem saepe, sit excepturi incidunt earum quis voluptates quia facere suscipit, dolor qui sint officiis sequi accusamus molestias ea repellat nam perferendis quod adipisci vero. Cumque, facere, ratione. Nostrum ratione laudantium dolore adipisci, ad, doloribus iste! Inventore temporibus, et voluptate dolores nesciunt, pariatur consequuntur! Ipsum obcaecati, ab culpa doloribus sint cumque fugiat cupiditate saepe enim nihil. Ipsa culpa mollitia quod, iure, doloremque autem architecto excepturi ab labore soluta aut cum nesciunt perferendis ad aperiam corrupti quibusdam! Nihil provident dolorum cum, dicta animi a officiis, aperiam beatae excepturi vero enim fuga amet hic corrupti quia tempore. Harum necessitatibus optio nemo cumque odit! Totam tenetur ea, quos unde nisi ad, iusto dolore ipsam aut ipsum tempora ex sunt rem praesentium placeat! Assumenda accusamus dolore quae, fugit placeat cum aliquam quod at eveniet sapiente molestias eos, officia omnis aspernatur modi veritatis eligendi minus repudiandae pariatur enim fugiat facilis odio sit nesciunt! Voluptate aliquid ut hic saepe vero itaque quisquam soluta aspernatur veritatis quos, provident quam asperiores, perferendis cupiditate delectus officiis amet aliquam corporis fugit, quaerat eveniet repudiandae. Voluptate esse vel iusto veritatis odio? Architecto aliquam natus quo inventore qui unde odio nemo quam! Fugiat labore quod praesentium? Molestias repudiandae mollitia, est ipsam vero sint aperiam, illo aliquam neque vel dolorum asperiores quo aliquid, natus harum aspernatur recusandae quam provident saepe dolore! Placeat sit consectetur harum incidunt nemo illo ipsum eveniet ratione in neque adipisci illum quas laborum eos optio alias porro accusamus fuga, voluptatibus facilis cupiditate rerum maiores quos tempora. Deserunt, quod?</p>
 
 
 
